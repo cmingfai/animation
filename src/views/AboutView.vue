bd046d1d8ef8a91d048b6387624f392b2b0b302e
Ming Fai Cheung <mingfai@live.com>
2023-11-28 17:45:13 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, maiores, veniam consectetur modi incidunt doloremque commodi sapiente non autem culpa totam iste asperiores doloribus odio distinctio unde porro eveniet eum.</p>
+  </div>
+</template>
+
+<style>
+
+  .about {
+    max-width: 300px;
+    margin: 10px auto;
+  }
+
+
+
+.container {
+  max-width: 300px;
+  margin: 0 auto;
+  
+}
+
+</style>
